fb520d3b4102cd614ab880819246fd5c49eeb5a7
phartenfeller <pharti@hotmail.de>
2020-02-12 12:41:38 +0100

    removed unused parser libraries

--- jison/parser.js
+++
@@ -1,9 +0,0 @@
-const readFile = require('../util/readFile');
-const jison = require('jison');
-
-const parserGenerator = async () => {
-  const bnf = await readFile('./jison/plsql.jison');
-  return new jison.Parser(bnf);
-};
-
-module.exports = parserGenerator;
--- src/lexer/definitions.js
+++
@@ -1,8 +0,0 @@
-const setupDefinitions = lexer => {
-  lexer.addDefinition('DIGIT', /[0-9]/);
-  lexer.addDefinition('BEGIN', /begin/);
-  lexer.addDefinition('END', /end/);
-  return lexer;
-};
-
-module.exports = setupDefinitions;
