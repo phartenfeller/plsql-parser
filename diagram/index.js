5f8c7eed77c3359ef99b58ae5488c6fb461d8cd3
phartenfeller <pharti@hotmail.de>
2020-05-25 18:54:49 +0200

    implemented chevrotain diagram feature

---
+++ diagram/index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+const chevrotain = require('chevrotain');
+const { parserInstance } = require('../chevrotain/rules');
+
+const serializedGrammar = parserInstance.getSerializedGastProductions();
+
+// create the HTML Text
+const htmlText = chevrotain.createSyntaxDiagramsCode(serializedGrammar);
+
+// Write the HTML file to disk
+const outPath = path.resolve(__dirname, './');
+fs.writeFileSync(`${outPath}/generated_diagrams.html`, htmlText);
